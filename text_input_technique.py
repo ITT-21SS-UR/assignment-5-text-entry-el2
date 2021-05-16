1557a0f1274074016e6715774169084632013b35
RealWhimsy <lukas.jack@web.de>
2021-05-16 15:42:41 +0200

    Added error highlighting and reading config from params

--- text_input_technique.py
+++ text_input_technique.py
@@ -15,10 +15,6 @@ import pandas as pd
 from datetime import datetime
 from PyQt5.QtWidgets import QTextEdit, QCompleter
 
-TEMPLATE_TEXT_LIST = ["Yep Yip sentence",
-                      "Here we have another wonderful sentence.",
-                      "Wow this is so much fun!"]
-
 CSV_HEADER = ["event", "user_id", "timestamp", "time_taken_in_ms", "auto_completed", "auto_completion_count",
               "wpm", "numBksp"]
 
@@ -36,6 +32,9 @@ class TextEdit(QTextEdit):
     Code taken from: https://github.com/baoboa/pyqt5/blob/master/examples/tools/customcompleter/customcompleter.py
     Slightly adjusted to our needs
     """
+
+    key_pressed_signal = QtCore.pyqtSignal(QtGui.QKeyEvent)
+
     def __init__(self, parent=None):
         super(TextEdit, self).__init__(parent)
 
@@ -85,6 +84,10 @@ class TextEdit(QTextEdit):
         super(TextEdit, self).focusInEvent(e)
 
     def keyPressEvent(self, e):
+        self.key_pressed_signal.emit(e)
+        if e.key() == Qt.Key_Return:
+            return 
+
         if self._completer is not None and self._completer.popup().isVisible():
             # The following keys are forwarded by the completer to the widget.
             if e.key() in (Qt.Key_Enter, Qt.Key_Return, Qt.Key_Escape, Qt.Key_Tab, Qt.Key_Backtab):
@@ -127,19 +130,26 @@ class TextEdit(QTextEdit):
 
 class SuperText(QtWidgets.QTextEdit):
 
+    sentence_list = []
+    original_text = ""
+    user_id = 0
+
     def __init__(self):
         super(SuperText, self).__init__()
         self.ui = uic.loadUi('text_entry_technique.ui')
         self.user_input_layout = self.ui.userInputLayout
         self.text_edit = TextEdit(self)
+        self.text_edit.key_pressed_signal.connect(self.handle_key_pressed)
         self.current_sentence_index = 0
-        self.original_text = TEMPLATE_TEXT_LIST[0]
+        self.parse_setup()
+        self.original_text = self.sentence_list[0]
         self.df = pd.DataFrame(columns=CSV_HEADER)
         self.initUI()
         self.sentence_timer = QtCore.QTime()
         self.word_timer = QtCore.QTime()
         self.key_timer = QtCore.QTime()
         self.sentence_timer_running = False
+        self.text_changed_by_user = True
         self.last_text_state = ""
         self.last_character_entered = ""
         self.num_backspace = 0
@@ -147,9 +157,27 @@ class SuperText(QtWidgets.QTextEdit):
         self.completer = ()
         self.init_completer()
 
+    def parse_setup(self):
+
+        if len(sys.argv) is not 3:
+            print("Please pass a .txt file with sentences and a user ID as arguments!")
+
+        else:
+            file = open(sys.argv[1], 'r')
+            lines = file.readlines()
+
+            for line in lines:
+                self.sentence_list.append(line.strip())
+
+            try:
+                self.user_id = int(sys.argv[2])
+            except ValueError:
+                print("Please provide a valid ID as second argument! Using fallback ID (0) instead.")
+                self.user_id = 0
+
     def init_completer(self):
         word_list = []
-        for sentence in TEMPLATE_TEXT_LIST:
+        for sentence in self.sentence_list:
             words = sentence.split()
             for word in words:
                 if word not in word_list:
@@ -173,6 +201,9 @@ class SuperText(QtWidgets.QTextEdit):
         self.text_edit.textChanged.connect(self.handleTextChanged)
 
     def handleTextChanged(self):
+        if not self.text_changed_by_user:
+            return
+
         if not self.sentence_timer_running:
             self.sentence_timer_running = True
             self.sentence_timer.start()
@@ -197,31 +228,37 @@ class SuperText(QtWidgets.QTextEdit):
             self.add_log_entry(EventType.WORD)
             self.word_count += 1
 
-        if self.last_character_entered == "\n":
+        # if self.last_character_entered == "\n":
+        #     self.handleSentenceFinished()
+
+    def handle_key_pressed(self, event):
+        self.text_changed_by_user = True
+
+        if event.key() == QtCore.Qt.Key_Return:
             self.handleSentenceFinished()
 
     def add_log_entry(self, event_type):
         if event_type == EventType.LETTER:
             self.df = self.df.append({
                 'event': "key_press",
-                'user_id': 1,
+                'user_id': self.user_id,
                 'timestamp': datetime.now(),
                 'time_taken_in_ms': self.key_timer.elapsed(),
                 'auto_completed': False,
                 'auto_completion_count': self.text_edit.auto_completion_count,
-                'wpm': 0,
+                'wpm': None,
                 'numBksp': self.num_backspace
             }, ignore_index=True)
 
         if event_type == EventType.WORD:
             self.df = self.df.append({
                 'event': "word",
-                'user_id': 1,
+                'user_id': self.user_id,
                 'timestamp': datetime.now(),
                 'time_taken_in_ms': self.word_timer.elapsed(),
                 'auto_completed': self.text_edit.auto_completed,
                 'auto_completion_count': self.text_edit.auto_completion_count,
-                'wpm': 0,
+                'wpm': None,
                 'numBksp': self.num_backspace
             }, ignore_index=True)
 
@@ -229,7 +266,7 @@ class SuperText(QtWidgets.QTextEdit):
             print(self.sentence_timer.elapsed())
             self.df = self.df.append({
                 'event': "sentence",
-                'user_id': 1,
+                'user_id': self.user_id,
                 'timestamp': datetime.now(),
                 'time_taken_in_ms': self.sentence_timer.elapsed(),
                 'auto_completed': self.text_edit.auto_completed,
@@ -244,15 +281,51 @@ class SuperText(QtWidgets.QTextEdit):
             self.sentence_timer_running = False
             self.text_edit.reset_auto_completion_stats()
 
-            if self.current_sentence_index == len(TEMPLATE_TEXT_LIST) - 1:
+            if self.current_sentence_index == len(self.sentence_list) - 1:
                 self.df.to_csv(sys.stdout, index=False)
                 self.ui.close()
             else:
                 self.show_new_sentence()
 
+        else:
+            self.highlight_errors()
+
+    def highlight_errors(self):
+        self.text_changed_by_user = False
+        text_format = QtGui.QTextCharFormat()
+        text_format.setBackground(QtGui.QBrush(QtGui.QColor("white")))
+        cursor = self.text_edit.textCursor()
+
+        user_text = self.text_edit.toPlainText()
+        original_text = self.original_text
+        cursor.setPosition(0)
+        cursor.movePosition(QtGui.QTextCursor.EndOfBlock, 1)
+        cursor.mergeCharFormat(text_format)
+
+        text_format.setBackground(QtGui.QBrush(QtGui.QColor("red")))
+
+        error_index_list = []
+        index = 0
+        while index < len(user_text):
+            if index >= len(original_text):
+                print("yep")
+                error_index_list.append(index)
+                index += 1
+                continue
+
+            if user_text[index] != original_text[index]:
+                error_index_list.append(index)
+
+            index += 1
+
+        for error_index in error_index_list:
+            cursor.setPosition(error_index)
+            cursor.movePosition(QtGui.QTextCursor.NextCharacter, 1)
+            cursor.mergeCharFormat(text_format)
+
     def show_new_sentence(self):
         self.current_sentence_index += 1
-        self.original_text = TEMPLATE_TEXT_LIST[self.current_sentence_index]
+        self.original_text = self.sentence_list[self.current_sentence_index]
         self.ui.originalText.setText(self.original_text)
         self.last_text_state = ""
         self.last_character_entered = ""
